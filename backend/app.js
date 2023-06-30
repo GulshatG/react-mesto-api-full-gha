1843f231b0574c23e1c4572c36ae910da6b58164
Mariam99999 <mariam_agadzhanyan@mail.ru>
2023-06-30 23:20:14 +0400

    fix CORS

--- backend/app.js
+++ backend/app.js
@@ -5,52 +5,44 @@ const cookieParser = require('cookie-parser');
 require('dotenv')
   .config();
 const helmet = require('helmet');
-const { errors } = require('celebrate');
+const {errors} = require('celebrate');
 const userRouter = require('./routes/userRouter');
 const cardRouter = require('./routes/cardRouter');
 const handleExceptions = require('./utils/handleException');
 const {
-  login,
-  createUser,
+  login, createUser,
 } = require('./cotrollers/userController');
-const { auth } = require('./middlewares/auth');
+const {auth} = require('./middlewares/auth');
 const {
-  celebrateCreateUser,
-  celebrateLogin,
+  celebrateCreateUser, celebrateLogin,
 } = require('./celebrate/celebrateUser');
 const PageNotFound = require('./exceptions/pageNotFound');
-const { pageNotFound } = require('./utils/validationMessage');
+const {pageNotFound} = require('./utils/validationMessage');
 
 // Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
+const {PORT = 3000} = process.env;
 
 const app = express();
 
-const allowedCors = [
-  'http://gulshat-express.nomoreparties.sbs',
-  'https://gulshat-express.nomoreparties.sbs',
-  'https://158.160.114.23',
-  'http://158.160.114.23',
-  'gulshat-express.nomoreparties.sbs'
-];
+const allowedCors = ['http://gulshat-express.nomoreparties.sbs', 'https://gulshat-express.nomoreparties.sbs',];
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 
 app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
+  const {origin} = req.headers;
+  const {method} = req;
 
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
-
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+
   return next();
 });
 
