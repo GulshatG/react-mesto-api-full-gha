7cf8bec634043e7d8fb49fd19f1c50953f68d9b0
Mariam99999 <mariam_agadzhanyan@mail.ru>
2023-07-02 23:41:01 +0400

    add serverConfig.js

---
+++ frontend/src/utils/ServerConfig.js
@@ -0,0 +1,4 @@
+const serverMod = 'prod';
+const serverUrl = 'https://api.gulshat-express.nomoreparties.sbs';
+const localhostUrl = 'http://localhost:3000';
+export {serverMod,serverUrl,localhostUrl};
